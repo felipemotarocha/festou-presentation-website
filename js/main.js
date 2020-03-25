791981e86d4a627bae2968c928ad14733580217b
Felipe Rocha <felipemota.rocha@gmail.com>
2020-03-25 01:00:34 -0300

    added responsiveness for mobile devices

--- js/main.js
+++ js/main.js
@@ -7,18 +7,18 @@ $("#navbar a, .btn-primary").on("click", function (event) {
 
         $("html, body").animate(
             {
-                scrollTop: $(hash).offset().top
+                scrollTop: $(hash).offset().top - 100
             },
             800
         );
     }
 });
 
-// // Sticky menu background
-// window.addEventListener("scroll", function() {
-//     if (window.scrollY > 150) {
-//         document.querySelector("#navbar").style.opacity = 0.9;
-//     } else {
-//         document.querySelector("#navbar").style.opacity = 1;
-//     }
-// });
+// Sticky menu background
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 150) {
+        document.querySelector("#navbar").style.opacity = 0.9;
+    } else {
+        document.querySelector("#navbar").style.opacity = 1;
+    }
+});
